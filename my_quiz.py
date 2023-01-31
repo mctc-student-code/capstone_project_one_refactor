8933ef4be2a0fa0c86a0d742a4621357fc221ee7
Clara <10088152+claraj@users.noreply.github.com>
2023-01-30 18:27:06 -0600

    feedback

--- my_quiz.py
+++ my_quiz.py
@@ -11,18 +11,25 @@ Due: 1/22/2023
 """
 
 # Store each quiz as a dictionary Keys = questions, Values = answers
-all_quizzes = {'Art':  {'Who painted the Mona Lisa?': 'Leonardo Da Vinci', 
-            'What precious stone is used to make the artist\'s pigment ultramarine?': 'Lapiz lazuli',
-            'Anish Kapoor\'s bean-shaped Cloud Gate sculpture is a landmark of which city?': 'Chicago',
-            'Which kid\'s TV characters are named after Renaissance artists?': 'Teenage Mutant Ninja Turtles',
-            'The graphite in an artist\'s pencil is made of what chemical element?': 'Carbon'}, 
-            'Space': {'Which planet is closest to the sun?': 'Mercury',
-            'Which planet spins in the opposite direction to all the others in the solar system?': 'Venus', 
-            'How many moons does Mars have?': '2'}, 
-            'Sports': {'Which gymnast is the "triple-twisting double-tucked salto backwards" skill named after?': 'Simone Biles',   
-            'Which country has won the soccer world cup the most times?': 'Brasil',   
-            'What does MLB stand for?': 'Major League Baseball'}}
-
+all_quizzes = {
+    'Art': {
+        'Who painted the Mona Lisa?': 'Leonardo Da Vinci', 
+        'What precious stone is used to make the artist\'s pigment ultramarine?': 'Lapiz lazuli',
+        'Anish Kapoor\'s bean-shaped Cloud Gate sculpture is a landmark of which city?': 'Chicago',
+        'Which kid\'s TV characters are named after Renaissance artists?': 'Teenage Mutant Ninja Turtles',
+        'The graphite in an artist\'s pencil is made of what chemical element?': 'Carbon'
+    }, 
+    'Space': {
+        'Which planet is closest to the sun?': 'Mercury',
+        'Which planet spins in the opposite direction to all the others in the solar system?': 'Venus', 
+        'How many moons does Mars have?': '2'
+    }, 
+    'Sports': {
+        'Which gymnast is the "triple-twisting double-tucked salto backwards" skill named after?': 'Simone Biles',   
+        'Which country has won the soccer world cup the most times?': 'Brasil',   
+        'What does MLB stand for?': 'Major League Baseball'
+    }
+}
 
 
 # Define a function that loops through the all_quizzes dictionary and prints the topic of each quiz
@@ -35,21 +42,28 @@ def print_topics():
     for quiz in all_quizzes:
         list_of_topics.append(quiz)
 
+    # Since the keys of the dictionary are the topics, you can simplfy to
+    list_of_topics = all_quizzes.keys()
+
     # Print the available topics, adding a comma to separate each
     print(', '.join(list_of_topics))
 
+
 # Define a function that uses the user's topic selection to deliver the correct quiz
+# return the quiz questions and answers
 def get_quiz_by_topic(user_topic):
 
     # Convert the user's input to title case to match topics
-    users_topic_title = str(user_topic).title()
+    users_topic_title = user_topic.title()
 
     # Using the user's topic selection, send the quiz to the take_quiz function
     user_quiz = all_quizzes.get(users_topic_title)
-    take_quiz(user_quiz)
+
+    return user_quiz
 
 
 # Loop through each question: answer and tally the points for the quiz
+# return the total questions asked, and the total points 
 def take_quiz(user_quiz):
     
     # Start counters for the number of correct answers and total number of questions
@@ -69,7 +83,7 @@ def take_quiz(user_quiz):
         user_answer = input().lower()
 
         # Convert the correct answer to lower to ignore case
-        correct_answer = str(answer).lower()
+        correct_answer = answer.lower()
 
         # Compare the user's answer to the correct answer while ignoring case
         if user_answer == correct_answer:
@@ -88,7 +102,9 @@ def take_quiz(user_quiz):
             print(f'I\'m sorry, the correct answer was {answer}')
 
     # Send the quiz results to be printed to the user
-    print_results(total_score, number_of_questions)
+    # better to write functions with defined input and output, easier to test. 
+    return total_score, number_of_questions
+
 
 # A function that uses the user's score and the total possible points to print the results
 def print_results(total_score, number_of_questions):
@@ -99,14 +115,26 @@ def print_results(total_score, number_of_questions):
         print(f'You got {total_score} of the {number_of_questions} questions correct.')
 
 
-# Ask the user what type of quiz they would like to take
-print('What type of quiz would you like to take? The topics are: ')
+# create a main function to define the starting point of the program 
 
-# Loop through the quiz list and print the topic of each quiz
-print_topics()
+def main():
+    # Ask the user what type of quiz they would like to take
+    print('What type of quiz would you like to take? The topics are: ')
 
-# Ask the user what type of quiz they want to take
-user_topic_selection = input()
+    # Loop through the quiz list and print the topic of each quiz
+    print_topics()
 
-# Use the user's choice to select the dictionary that has the correct topic
-get_quiz_by_topic(user_topic_selection)
+    # Ask the user what type of quiz they want to take
+    user_topic_selection = input()  # add validation here to prevent user entering invalid topic, perhaps in a new function 
+
+    # call the functions that represent the parts of the quizzing process in order 
+    # Eaasier to look at main and see the flow through the program and order of operations 
+
+    # Use the user's choice to select the dictionary that has the correct topic
+    quiz = get_quiz_by_topic(user_topic_selection)
+
+    total_score, number_of_questions = take_quiz(quiz)    
+
+    print_results(total_score, number_of_questions)
+
+main()
